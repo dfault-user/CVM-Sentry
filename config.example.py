e3c4e7c74eaa7045ac4322e934c8bcd5bf40d450
Clair Delafuente <clair@nixlabs.dev>
2025-09-30 15:26:13 -0400

    Example config

---
+++ config.example.py
@@ -0,0 +1,17 @@
+credentials = {
+    "session_auth": "", # Supply session auth
+}
+
+log_level = "DEBUG"
+
+vms = {
+    "vm1": "wss://computernewb.com/collab-vm/vm1",
+    "vm2": "wss://computernewb.com/collab-vm/vm2",
+    "vm3": "wss://computernewb.com/collab-vm/vm3",
+    "vm4": "wss://computernewb.com/collab-vm/vm4",
+    "vm5": "wss://computernewb.com/collab-vm/vm5",
+    "vm6": "wss://computernewb.com/collab-vm/vm6",
+    "vm7": "wss://computernewb.com/collab-vm/vm7",
+}
+
+noauth_name = "CVMSentry" #
