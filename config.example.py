7f03d10fa23e20e42c3ce775123a084b0c3c00f4
Clair Delafuente <clair@nixlabs.dev>
2025-09-30 23:12:16 -0400

    update config example

--- config.example.py
+++ config.example.py
@@ -5,6 +5,8 @@ credentials = {
 log_level = "INFO"
 log_directory = "./logs"
 
+auth_server = "https://auth.collabvm.org"
+
 vms = {
     "vm1": "wss://computernewb.com/collab-vm/vm1",
     "vm2": "wss://computernewb.com/collab-vm/vm2",
@@ -15,4 +17,9 @@ vms = {
     "vm7": "wss://computernewb.com/collab-vm/vm7",
     "vm8": "wss://computernewb.com/collab-vm/vm8",
     "vm9": "wss://computernewb.com/collab-vm/vm9",
+}
+
+commands = {
+    "enabled": True,
+    "prefix": "+",
 }
